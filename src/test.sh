5313aafc48bda1703ceef7feb04a37edddd1ba6d
salventre <s.ventre@studenti.unisa.it>
2022-08-08 23:09:09 +0200

    Modified .sh files

--- src/test.sh
+++ src/test.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-export CUDA_VISIBLE_DEVICES=3
-nohup python3 src/generate_results.py --dataset src/partition_info/test_filenames.txt --output_path ./data/doc/results.txt --model_path ./data/checkpoint/model-epoch_05.hdf5 > log_test.txt
+CUDA_VISIBLE_DEVICES=3 nohup python3 src/generate_results.py --dataset src/partition_info/test_filenames.txt --output_path ./data/doc/results.txt --model_path ./data/checkpoint/model-epoch_05.hdf5 > log_test.txt &
--- src/train.sh
+++ src/train.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-export CUDA_VISIBLE_DEVICES=3
-nohup python3 src/training.py --train_path ./data/dataset/train/ --epochs 1 > log_train.txt &
+CUDA_VISIBLE_DEVICES=3 nohup python3 src/training.py --train_path ./data/dataset/train/ --epochs 1 > log_train.txt &
 nohup python3 utils/bot.py > bot.txt &
