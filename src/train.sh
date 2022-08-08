b8c6bf09466038adb23768b6b3d7159c0d636c54
salventre <s.ventre@studenti.unisa.it>
2022-08-08 19:03:44 +0200

    train.sh modified for bot

--- src/train.sh
+++ src/train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=3
-python3 src/training.py --train_path ./data/dataset/train/ --epochs 1
-#nohup python3 src/bot.py > bot.txt &
+nohup python3 src/training.py --train_path ./data/dataset/train/ --epochs 1 > log_train.txt &
+nohup python3 utils/bot.py > bot.txt &
