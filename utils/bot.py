22197e129c8a0219499cea174100f8b6e8e50b6e
salvatoreventre <58272078+salventre@users.noreply.github.com>
2022-11-04 19:34:22 +0100

    Update bot.py

--- utils/bot.py
+++ utils/bot.py
@@ -5,7 +5,7 @@ import os
 sys.path.insert(0, os.path.abspath("../weather-camera-thesis/src"))
 from plot_hist import plot_hist
 
-TOKEN="5528518099:AAHhPoPkCxt4-Kf6W_ed7oqkq52M1OQ4M54" #to check
+TOKEN="?" #to check
 
 n_experiment = "11_experiment" #specify the expriment to visualize
 CSV_PATH = "'/user/sventre/Weather_Camera/Project Saves/{}/logs/training_log.csv".format(n_experiment)
